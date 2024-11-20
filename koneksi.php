7393d6c5439b6ec3dfdd653f1cd6ac36052ea5ea
putri-nayla <putri20820@gmail.com>
2024-11-20 09:05:27 +0700

    membuat crud

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+$server="localhost";
+$user_db="root";
+$pass_db="";
+$db_name="crudputri";
+$koneksi = mysqli_connect($server,$user_db,$pass_db,$db_name);
+if(mysqli_connect_error()){
+    echo "koneksi Gagal:".mysqli_connect_error();
+}
---
+++ views/admin.php
@@ -0,0 +1,94 @@
+<!-- Default box -->
+<div class="card">
+</div>
+<!--/.card-->
+        <div class="card-header">
+          <h3 class="card-title">admin</h3>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+              <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+              <i class="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+        <div class="card-body">
+          admin<table id="example1" class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
+                    <th>Rendering engine</th>
+                    <th>Browser</th>
+                    <th>Platform(s)</th>
+                    <th>Engine version</th>
+                    <th>CSS grade</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr>
+                    <td>Trident</td>
+                    <td>Internet
+                      Explorer 4.0
+                    </td>
+                    <td>Win 95+</td>
+                    <td> 4</td>
+                    <td>X</td>
+                  </tr>
+                  <tr>
+                    <td>Trident</td>
+                    <td>Internet
+                      Explorer 5.0
+                    </td>
+                    <td>Win 95+</td>
+                    <td>5</td>
+                    <td>C</td>
+                  </tr>
+                  <tr>
+                    <td>Trident</td>
+                    <td>Internet
+                      Explorer 5.5
+                    </td>
+                    <td>Win 95+</td>
+                    <td>5.5</td>
+                    <td>A</td>
+                  </tr>
+                  <tr>
+                    <td>Trident</td>
+                    <td>Internet
+                      Explorer 6
+                    </td>
+                    <td>Win 98+</td>
+                    <td>6</td>
+                    <td>A</td>
+                  </tr>
+                  <tr>
+                    <td>Trident</td>
+                    <td>Internet Explorer 7</td>
+                    <td>Win XP SP2+</td>
+                    <td>7</td>
+                    <td>A</td>
+                  </tr>
+                  <tr>
+                    <td>Trident</td>
+                    <td>AOL browser (AOL desktop)</td>
+                    <td>Win XP</td>
+                    <td>6</td>
+                    <td>A</td>
+                  </tr>
+                  <tr>
+                    <td>Gecko</td>
+                    <td>Firefox 1.0</td>
+                    <td>Win 98+ / OSX.2+</td>
+                    <td>1.7</td>
+                    <td>A</td>
+                  </tr>
+</body>
+        </div>
+        <!-- /.card-body -->
+        <!--<div class="card-footer">
+          Footer
+        </div>-->
+        <!-- /.card-footer-->
+      </div>
+      <!-- /.card -->
